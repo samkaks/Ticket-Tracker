2c5d07a03d9bfd94eb29e94e7f1afcd87b4e5f00
Samarth Kakria <samarthkakria@Samarths-MacBook-Pro.local>
2014-04-29 15:39:53 -0400

    Initial Commit

---
+++ Ticket Tracker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Ticket Tracker
+//
+//  Created by Samarth Kakria on 4/29/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Ticket Tracker/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Ticket Tracker
+//
+//  Created by Samarth Kakria on 4/29/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
